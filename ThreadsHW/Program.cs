9278cbd20622b1c3b266dca41db509e4e4c22d3e
Tixon9I <oksana.andrusenko12345@gmail.com>
2024-09-05 14:27:56 +0300

    Add project files.

---
+++ ThreadsHW/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ThreadsHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+        }
+    }
+}
